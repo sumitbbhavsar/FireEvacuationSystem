3703a9d10f412a678fb0bd0f0d26b1b5e87c5996
Sumit Bhavsar <sumit.bhavsar@capgemini.com>
2019-04-22 14:35:21 +0530

    API changes done.

--- FireEvacuationSystem.API/Models/EmployeeRoutes.cs
+++ FireEvacuationSystem.API/Models/EmployeeRoutes.cs
@@ -4,4 +4,14 @@ public class EmployeeRoutes :EmployeeRoutesRequest
     /// Get or sets Id
     /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Get or sets Id EmployeeName
+    /// </summary>
+    public string EmployeeName {get;set;}
+
+    /// <summary>
+    /// Get or sets Block Name
+    /// </summary>
+    public string BlockName { get; set; }
 }
