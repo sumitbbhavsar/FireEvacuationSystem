7298c396f17b51935c4f7f823a2a1a3b526e7a80
Akshay Chavda <akshaychavda10@gmail.com>
2019-04-25 16:37:44 +0530

    Commited

--- FireEvacuationSystem.Simulator/Program.cs
+++ FireEvacuationSystem.Simulator/Program.cs
@@ -15,22 +15,27 @@ namespace DataPusherApp
 
        public static void Main()
         {
+            string isCordinate="0";
+            isCordinate = Console.ReadLine();
             var OemployeeList =new EmployeeList();
 
             int NoOfRouters = 3;//dB values (select top 3 router from tableRouter order by SignalStrength desc)
             double[] freqInMHz  = new double[]{2412,2412,2412}; //mHz from DB values
 
-            var jsonData = System.IO.File.ReadAllText(@"D:/Users/akchavda/OneDrive - Capgemini\DataPusherApp/data/EmployeeSignalStrength.json");
-            
-            List<Employee> empList = JsonConvert.DeserializeObject<List<Employee>>(jsonData);
+           
 
-            
-            for (int i = 0; i < empList.Count; i++)
+            if (isCordinate=="0")
             {
-                empList[i].s1List=empList[i].SignalStrength1.Split(",").ToList();
-                empList[i].s2List=empList[i].SignalStrength2.Split(",").ToList();
-                empList[i].s3List=empList[i].SignalStrength3.Split(",").ToList();                            
-            }
+                #region 
+                var jsonData = System.IO.File.ReadAllText(@"D:\Akshay\FireEvacuationSystem\FireEvacuationSystem.Simulator\data\EmployeeSignalStrength.json");
+            
+                List<Employee> empList = JsonConvert.DeserializeObject<List<Employee>>(jsonData);
+                for (int i = 0; i < empList.Count; i++)
+                {
+                    empList[i].s1List=empList[i].SignalStrength1.Split(",").ToList();
+                    empList[i].s2List=empList[i].SignalStrength2.Split(",").ToList();
+                    empList[i].s3List=empList[i].SignalStrength3.Split(",").ToList();                            
+                }
 
             
                 for (int j=0; j < empList[0].s1List.Count;j++)
@@ -70,12 +75,69 @@ namespace DataPusherApp
                         {
                             Console.WriteLine("API called " + response.StatusCode)   ;
                         }
+                        else
+                        {
+                                Console.WriteLine("API called " + response.StatusCode)   ;
+                        }
                     }
-                    System.Threading.Thread.Sleep(2000);
+                    System.Threading.Thread.Sleep(1000);
                 }
 
-                    }
+            }
+            #endregion
+            }
+            else
+            {
+               var jsonData = System.IO.File.ReadAllText(@"D:\Akshay\FireEvacuationSystem\FireEvacuationSystem.Simulator\data\EmployeeSignalStrength_Cordinates.json");
+            
+                List<EmployeeRoutesRequestWithCordinate> empList = JsonConvert.DeserializeObject<List<EmployeeRoutesRequestWithCordinate>>(jsonData);
+                for (int i = 0; i < empList.Count; i++)
+                {
+                    empList[i].XList=empList[i].X.Split(",").ToList();
+                    empList[i].YList=empList[i].Y.Split(",").ToList();
                 }
+            
+                for (int j=0; j < empList[0].XList.Count;j++)
+                {
+                    for (int emp=0; emp < empList.Count;emp++)
+                    {
+                        double s1=Convert.ToDouble(empList[emp].XList[j]);
+                        double s2=Convert.ToDouble(empList[emp].YList[j]);
+                        int  empId=(empList[emp].EmployeeId);
+
+                        double[] SignalStrengthInDb = new double[]{s1,s2};
+                        using (HttpClient client = new HttpClient())
+                        {
+                            
+                            EmployeeRoutesRequest request =  new  EmployeeRoutesRequest();
+                            request.X=SignalStrengthInDb[0];
+                            request.Y=SignalStrengthInDb[1];
+                            request.EmployeeId=empId;
+                            request.NearByRouteId=8;
+                            Console.WriteLine("API called " + empId);
+                            StringContent content = new StringContent (JsonConvert.SerializeObject(request),Encoding.UTF8,"application/json");
+                            HttpResponseMessage response =  client.PostAsync("http://localhost:5000/api/EmployeeRoutes", content).Result;
+
+                            if(response.IsSuccessStatusCode)
+                            {
+                                Console.WriteLine("API called " + request.X);
+                                Console.WriteLine("API called " + request.Y);
+                                Console.WriteLine("API called " + response.StatusCode);
+                            }
+                            else
+                            {
+                                Console.WriteLine("API called " + request.X);
+                                Console.WriteLine("API called " + request.Y);
+                                Console.WriteLine("API called " + response.StatusCode);
+                            }
+                        }
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+            
+            
 
             
             // #region looping through each employee and getting x & y from signal strength
@@ -125,25 +187,25 @@ namespace DataPusherApp
         public static double[] GetEmpLocationByRadii(double[] radii)
         {
             //Test
-		double dbX=10;//get from DB for dynamic Room Size
-		double dbY=10;//get from DB for dynamic Room Size
-		
-		double[] locate = new double[]{0,0};
-		double r1 = radii[0]; //Radius of Circle 1 Origin
-		double r2 = radii[1]; //Radius of Circle 2 X-Axis
-		double r3 = radii[2]; //Radius of Circle 3 X-Axis
-		double x; // Emp Location Co-Ordinate X
-		double y;// Emp Location Co-Ordinate Y
-		//add X Cordiante
-		
-		x = Math.Abs(((r1*r1)-(r2*r2)+(dbX*dbX))/(2*dbX));
-		
-		//add Y Cordiante
-		y = Math.Abs(((r1*r1)-(r3*r3)+(dbY*dbY))/(2*dbY));
-		
-		locate[0]=Math.Round(x,1);
-		locate[1]=Math.Round(y,1);
-		return locate;
+            double dbX=10;//get from DB for dynamic Room Size
+            double dbY=10;//get from DB for dynamic Room Size
+            
+            double[] locate = new double[]{0,0};
+            double r1 = radii[0]; //Radius of Circle 1 Origin
+            double r2 = radii[1]; //Radius of Circle 2 X-Axis
+            double r3 = radii[2]; //Radius of Circle 3 X-Axis
+            double x; // Emp Location Co-Ordinate X
+            double y;// Emp Location Co-Ordinate Y
+            //add X Cordiante
+            
+            x = Math.Abs(((r1*r1)-(r2*r2)+(dbX*dbX))/(2*dbX));
+            
+            //add Y Cordiante
+            y = Math.Abs(((r1*r1)-(r3*r3)+(dbY*dbY))/(2*dbY));
+            
+            locate[0]=Math.Round(x,1);
+            locate[1]=Math.Round(y,1);
+            return locate;
         }
         #endregion
     }
@@ -172,30 +234,65 @@ namespace DataPusherApp
     #region EmployeeRoutesRequest
     public class EmployeeRoutesRequest
     {
-    /// <summary>
-    /// Get or sets X
-    /// </summary>
-    public double X { get; set; }
+        /// <summary>
+        /// Get or sets X
+        /// </summary>
+        public double X { get; set; }
 
-    /// <summary>
-    /// Get or sets Y
-    /// </summary>
-    public double Y { get; set; }
+        /// <summary>
+        /// Get or sets Y
+        /// </summary>
+        public double Y { get; set; }
 
-    /// <summary>
-    /// Get or sets NearByRouteId
-    /// </summary>
-    public int NearByRouteId { get; set; }
+        /// <summary>
+        /// Get or sets NearByRouteId
+        /// </summary>
+        public int NearByRouteId { get; set; }
 
-    /// <summary>
-    /// Get or sets EmployeeId
-    /// </summary>
-    public int EmployeeId { get; set; }
+        /// <summary>
+        /// Get or sets EmployeeId
+        /// </summary>
+        public int EmployeeId { get; set; }
 
-    /// <summary>
-    /// Get or sets TimeStamp 
-    /// </summary>
-    public DateTime TimeStamp {get;set;}
+        /// <summary>
+        /// Get or sets TimeStamp 
+        /// </summary>
+        public DateTime TimeStamp {get;set;}
+    } 
+    #endregion
+     #region EmployeeRoutesRequest
+    public class EmployeeRoutesRequestWithCordinate
+    {
+        /// <summary>
+        /// Get or sets X
+        /// </summary>
+        public string X { get; set; }
+
+        /// <summary>
+        /// Get or sets Y
+        /// </summary>
+        public string Y { get; set; }
+
+        /// <summary>
+        /// Get or sets NearByRouteId
+        /// </summary>
+        public int NearByRouteId { get; set; }
+
+        /// <summary>
+        /// Get or sets EmployeeId
+        /// </summary>
+        public int EmployeeId { get; set; }
+        /// <summary>
+        /// Get or sets EmployeeId
+        /// </summary>
+        public int EmpID { get; set; }
+
+        /// <summary>
+        /// Get or sets TimeStamp 
+        /// </summary>
+        public DateTime TimeStamp {get;set;}
+        public List<string> XList {get;set;}
+        public List<string> YList {get;set;}
     } 
     #endregion
 }
