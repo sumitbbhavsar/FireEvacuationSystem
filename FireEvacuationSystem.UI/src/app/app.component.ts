7c5aa2e930d58974c2e17c45a564174d7f239e09
Tushar Gupta <mca09mc27@gmail.com>
2019-04-25 14:29:17 +0530

    Image Integartion

--- FireEvacuationSystem.UI/src/app/app.component.ts
+++ FireEvacuationSystem.UI/src/app/app.component.ts
@@ -70,7 +70,7 @@ columnNames = ['Name', 'X','Y'];
   options = {
     vAxis: {viewWindow: {min: 0, max: 100}, format: "#", gridlines: {count: 5}},
     hAxis: {viewWindow: {min: 0, max: 100}, format: "#", gridlines: {count: 5}},
-     chartArea:{backgroundColor: '#fdc', width:1200, height:600},
+     chartArea:{ width:1200, height:600},
          };
    width = 1400;
    height = 700;
